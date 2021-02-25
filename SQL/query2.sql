02f87c8acd432279fa31ec9a028d537576c14277
kshitijjagatkar <kshitijjagatkar505@gmail.com>
2021-02-25 13:22:40 +0530

    sql

--- SQL/query2.sql
+++
@@ -1,9 +0,0 @@
--- c. Write a SQL query to count the total number of products per warehouse?
-
-        select w.w_name, COUNT(p_name) AS product_count
-        FROM stocks AS s
-        JOIN wearhouse as w
-            ON w.w_id = s.wh_id
-        JOIN product AS p
-            ON p.p_id = s.p_id
-        GROUP BY 1;
